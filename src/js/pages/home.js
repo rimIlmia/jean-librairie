815da44a6ce392697cd3c0c76369724e2ccb1dee
rimIlmia <rim.chakroun.pro@gmail.com>
2020-09-02 17:02:05 +0200

    delete books component de home

--- src/js/pages/home.js
+++ src/js/pages/home.js
@@ -1,12 +1,10 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import Books from "../../js/component/books";
 
 const Home = () => {
   return (
     <div>
       <h1>Welcome to library</h1>
-      <Books />
     </div>
   );
 };
